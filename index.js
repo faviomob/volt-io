e8298ad0b9390ad618c6a24d7e782c5da92555d2
Marat <m.alekperov@gmail.com>
2017-09-13 23:25:30 +0800

    index.js fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io');
+var io = require('socket.io')(server);
 var port = process.env.PORT || 5000;
 
 server.listen(port, function () {
